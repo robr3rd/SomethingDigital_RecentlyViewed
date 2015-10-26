37b6b4c7dd021daa941a9d4065c7b7e53da49b3d
Robert Robinson <robr3rd@gmail.com>
2015-10-26 15:26:57 -0400

    Implement support for the insert_position admin setting.

--- app/code/community/SomethingDigital/RecentlyViewed/Model/Adminhtml/System/Config/SdRecentlyViewedPosition.php
+++ app/code/community/SomethingDigital/RecentlyViewed/Model/Adminhtml/System/Config/SdRecentlyViewedPosition.php
@@ -6,10 +6,10 @@ class SomethingDigital_RecentlyViewed_Model_Adminhtml_System_Config_sdRecentlyVi
        $position = array(
            array( 'value' => 'top',    'label' => 'Top'    ),
            array( 'value' => 'bottom', 'label' => 'Bottom' ),
-           array( 'value' => 'above',  'label' => 'Above'  ),
-           array( 'value' => 'below',  'label' => 'Below'  ),
+           array( 'value' => 'before',  'label' => 'Above'  ),
+           array( 'value' => 'after',  'label' => 'Below'  ),
        );
- 
+
        return $position;
    }
 }
